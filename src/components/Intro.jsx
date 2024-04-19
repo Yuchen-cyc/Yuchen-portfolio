7036e8403bcc1da5178645c5eb2d1cf48a04f988
Yuchen Chen <joyce.yuchen.chen@gmail.com>
2024-04-19 16:38:02 +0200

    Intro right side's links

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Intro.css'
+import Github from '../img/github.png'
+import Linkedin from '../img/linkedin.png'
+import Instagram from '../img/instagram.png'
 
 export default function Intro() {
   return (
@@ -13,6 +16,17 @@ export default function Intro() {
           </span>
         </div>
         <button className='btn intro-btn'>Hire me</button>
+        <div className="intro-link">
+          <a href="https://github.com/Yuchen-cyc" target="_blank" rel="noopener noreferrer">
+            <img src={Github} alt="" className='intro-link-img'/>
+          </a>
+          <a href="https://www.linkedin.com/in/yuchen-chen-joyce/" target="_blank" rel="noopener noreferrer">
+            <img src={Linkedin} alt="" className='intro-link-img'/>
+          </a>
+          <a href="https://www.instagram.com/joyce.cyc/" target="_blank" rel="noopener noreferrer">
+            <img src={Instagram} alt="" className='intro-link-img'/>
+          </a>
+        </div>
       </div>
       <div className="intro-right">
         I am the right side.
