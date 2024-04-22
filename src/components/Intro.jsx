7a61ca8fc1e43d95177d8730394893e88482764f
Yuchen Chen <joyce.yuchen.chen@gmail.com>
2024-04-22 17:27:58 +0200

    Finish the floatingDiv component of the right intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './Intro.css'
+import FloatingDiv from './FloatingDiv'
 import Github from '../img/github.png'
 import Linkedin from '../img/linkedin.png'
 import Instagram from '../img/instagram.png'
+import Vector1 from '../img/Vector1.png'
+import Vector2 from '../img/Vector2.png'
+import Boy from '../img/boy.png'
+import Thumbup from '../img/thumbup.png'
+import Crown from '../img/crown.png'
+import glassesimoji from '../img/glassesimoji.png'
 
 export default function Intro() {
   return (
@@ -29,7 +36,16 @@ export default function Intro() {
         </div>
       </div>
       <div className="intro-right">
-        I am the right side.
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={Boy} alt="" />
+        <img src={glassesimoji} alt="" />
+        <div style={{top: '-4%', left: '58%'}}>
+          <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+        </div>
+        <div style={{top: '18.2rem', left: '1.2rem'}}>
+          <FloatingDiv image={Thumbup} txt1='Front-end' txt2='Developer'/>
+        </div>
       </div>
     </div>
   )
