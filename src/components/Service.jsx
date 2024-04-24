3c16aea3488c809fa3e65df508a86108912d22f9
Yuchen Chen <joyce.yuchen.chen@gmail.com>
2024-04-24 15:47:44 +0200

    Complete Service section

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Service.css'
 import Card from './Card'
 import Heartemoji from '../img/heartemoji.png'
+import Glasses from '../img/glasses.png'
+import Humble from '../img/humble.png'
+import ResumeCYC from './resumeCYC.pdf'
 
 export default function Service() {
   return (
@@ -14,18 +17,38 @@ export default function Service() {
           <br />
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </span>
-        <button className='btn service-btn'>Download CV</button>
-        <div className="blur service-blur" style={{background: '#ABF1FF94'}}></div>
+        <a href={ResumeCYC} download>
+          <button className='btn service-btn'>Download CV</button>
+        </a>
+        <div className="blur service-blur1" style={{background: '#ABF1FF94'}}></div>
       </div>
       {/* right side */}
       <div className="service-right">
+        {/* first card */}
         <div style={ {left: "14rem"} }>
           <Card
             emoji={Heartemoji}
             heading='Design'
-            detail='Figma, UI/UX, Sketch, Photoshop, Adobe Suite'
+            detail='Figma, Sketch, Photoshop, Adobe Suite'
           />
         </div>
+        {/* second card */}
+        <div style={{top: '12rem', left: '-4rem'}}>
+          <Card
+            emoji={Glasses}
+            heading={'Developer'}
+            detail={'HTML, CSS, JavaScript, React, Ruby on Rails'}
+          />
+        </div>
+        {/* third card */}
+        <div style={{top: '19rem', left: '12rem'}}>
+          <Card
+            emoji={Humble}
+            heading={'UI/UX'}
+            detail={'User friendly, Smooth user interface'}
+          />
+        </div>
+        <div className="blur service-blur2" style={{background: 'var(--purple)'}}></div>
       </div>
     </div>
   )
