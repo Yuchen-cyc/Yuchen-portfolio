268b1afd6222a4fa4d15ee5a969f6d0b4a3c9049
Yuchen Chen <joyce.yuchen.chen@gmail.com>
2024-04-30 11:30:08 +0200

    Completed the Contact section and connected to Emailjs successfully

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const form = useRef();
+  const [done, setDone] = useState(false)
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_xlmpzga', 'template_oxd0fkq', form.current, {
+        publicKey: 'oyKcne94HQKduT6hM',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          setDone(true);
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div className='contact-form'>
       <div className="contact-left">
@@ -13,11 +36,12 @@ export default function Contact() {
       </div>
 
       <div className="contact-right">
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='user_name' className='user' placeholder='Name' />
           <input type="text" name='user_email' className='user' placeholder='Email' />
           <textarea name="message" className='user' placeholder='Message'></textarea>
           <input type="submit" value="Send" className='btn contact-btn' />
+          <span className='form-confirmation'>{done && "Thanks for contacting me!"}</span>
           <div className="blur contact-blur1" style={{background: 'var(--purple)'}}></div>
         </form>
       </div>
